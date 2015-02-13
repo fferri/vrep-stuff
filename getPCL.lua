a9cd4e382379af017bf8e4b144eb4caddf77b93e
Federico Ferri <federico.ferri.it@gmail.com>
2015-02-13 12:15:04 +0100

    split getting pcl and saving pcl

---
+++ getPCL.lua
@@ -0,0 +1,24 @@
+getPCL=function(visSensorHandle)
+    local data={}
+    local r=0
+    local t1={}
+    local u1={}
+    r,t1,u1=simReadVisionSensor(visSensorHandle) -- discard first reading
+    r,t1,u1=simReadVisionSensor(visSensorHandle)
+    local o=simGetObjectPosition(visSensorHandle,-1)
+    local q=simGetObjectQuaternion(visSensorHandle,-1)
+    local m1=simGetObjectMatrix(visSensorHandle,-1)
+    if u1 then
+        for j=0,u1[2]-1,1 do
+            for i=0,u1[1]-1,1 do
+                local w=2+4*(j*u1[1]+i)
+                if u1[w+4]<(maxDist*0.9999) then
+                    local p={u1[w+1],u1[w+2],u1[w+3]}
+                    p=simMultiplyVector(m1,p)
+                    for h=1,3,1 do table.insert(data,p[h]) end
+                end
+            end
+        end
+    end
+    return data
+end
