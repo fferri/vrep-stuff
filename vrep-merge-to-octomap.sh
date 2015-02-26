f1bbaa774289f9ae96194a3365d1685e12aa97d7
Federico Ferri <federico.ferri.it@gmail.com>
2015-02-26 16:52:46 +0100

    parse tolerance with -t

--- vrep-merge-to-octomap.sh
+++ vrep-merge-to-octomap.sh
@@ -2,18 +2,24 @@
 
 set -e
 
-tol=${1:-0.01}
-outdir=vrep-dynpcl-dataset
+tol=0.01
+
+if [ "$1" = "-t" ]; then
+    shift
+    tol=$1
+    shift
+fi
+
+indir=raw
+outdir=dataset
 
 p_miss=0.0
 p_hit=1.0
 
 mkdir -p $outdir
 
-./pcl-boolean-op/pcl_make_scanlog.sh vrep-pcl-dyn1-{0..150}.pcd vrep-pcl-dyn2-0.pcd > $outdir/scan.log
-./octomap/bin/log2graph $outdir/scan.log $outdir/scan.graph
+pcl_make_scanlog vrep-pcl-dyn1-{0..150}.pcd vrep-pcl-dyn2-0.pcd > $outdir/scan.log
+~/octomap/bin/log2graph $outdir/scan.log $outdir/scan.graph
 rm $outdir/scan.log
-./octomap/bin/graph2tree -res $tol -g -i $outdir/scan.graph -o $outdir/octomap.bt -sensor $p_miss $p_hit
-
-./octomap/bin/octovis $outdir/octomap.bt &
+~/octomap/bin/graph2tree -res $tol -g -i $outdir/scan.graph -o $outdir/octomap.bt -sensor $p_miss $p_hit
 
