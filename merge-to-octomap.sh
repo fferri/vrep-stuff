754ab9c0d1b8ae5c5174ea0f85d884a082353287
Federico Ferri <federico.ferri.it@gmail.com>
2015-02-26 17:18:23 +0100

    build file list incrementally, plus other small changes

--- merge-to-octomap.sh
+++ merge-to-octomap.sh
@@ -11,15 +11,23 @@ if [ "$1" = "-t" ]; then
 fi
 
 indir=raw
-outdir=dataset
 
 p_miss=0.0
 p_hit=1.0
 
-mkdir -p $outdir
+pcd_list() {
+    local i
+    for k in dyn1 dyn2; do
+        i=0
+        while [ -f $indir/vrep-pcl-$k-$i.pcd ]; do
+            echo $indir/vrep-pcl-$k-$i.pcd
+            i=$(($i + 1))
+        done
+    done
+}
 
-pcl_make_scanlog vrep-pcl-dyn1-{0..150}.pcd vrep-pcl-dyn2-0.pcd > $outdir/scan.log
-~/octomap/bin/log2graph $outdir/scan.log $outdir/scan.graph
-rm $outdir/scan.log
-~/octomap/bin/graph2tree -res $tol -g -i $outdir/scan.graph -o $outdir/octomap.bt -sensor $p_miss $p_hit
+pcl_make_scanlog $(pcd_list) > scan.log
+~/octomap/bin/log2graph scan.log scan.graph
+rm scan.log
+~/octomap/bin/graph2tree -res $tol -g -i scan.graph -o octomap.bt -sensor $p_miss $p_hit
 
